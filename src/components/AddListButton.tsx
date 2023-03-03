f03372886783325a099065b09cf6ddd5a5cbdd66
Daria Czupreta <daria.czupreta@gmail.com>
2023-03-03 19:37:39 +0100

    Added adding list button

---
+++ src/components/AddListButton.tsx
@@ -0,0 +1,47 @@
+import { AddIcon } from "@chakra-ui/icons";
+import { Card, CardBody, Text } from "@chakra-ui/react";
+
+const AddListButton = ({
+  setActiveAddList,
+  activeAddList,
+}: {
+  setActiveAddList: React.Dispatch<React.SetStateAction<boolean>>;
+  activeAddList: boolean;
+}) => {
+  return (
+    <Card
+      onClick={() => setActiveAddList(!activeAddList)}
+      maxW="231px"
+      style={{ marginLeft: "20px", background: "rgba(204, 204, 204, 0.5)" }}
+      shadow="2px"
+    >
+      <CardBody
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "left",
+          paddingTop: 15,
+          paddingBottom: 15,
+          paddingRight: 30,
+          paddingLeft: 30,
+          cursor: "pointer",
+        }}
+      >
+        <AddIcon
+          w={2.5}
+          marginRight={"10px"}
+          style={{ color: "whitesmoke", position: "relative" }}
+        />
+        <Text
+          as="b"
+          fontSize="sm"
+          style={{ color: "whitesmoke", position: "relative" }}
+        >
+          Add another list
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default AddListButton;
---
+++ src/components/AddListField.tsx
@@ -0,0 +1,87 @@
+import { AddIcon } from "@chakra-ui/icons";
+import { Button, Card, CardBody, Input } from "@chakra-ui/react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+
+const Footer = styled.div``;
+type Inputs = {
+  title: string;
+};
+const AddListField = ({
+  setActiveAddList,
+  activeAddList,
+}: {
+  setActiveAddList: React.Dispatch<React.SetStateAction<boolean>>;
+  activeAddList: boolean;
+}) => {
+  useEffect(() => {
+    setFocus("title");
+  }, []);
+
+  const {
+    setValue,
+    register,
+    setFocus,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  return (
+    <Card
+      maxW="231px"
+      style={{ marginLeft: "20px", background: "rgba(204, 204, 204)" }}
+      shadow="2px"
+    >
+      <CardBody
+        style={{
+          alignItems: "center",
+          justifyContent: "left",
+          paddingTop: 15,
+          paddingBottom: 15,
+          paddingRight: 15,
+          paddingLeft: 15,
+          cursor: "pointer",
+        }}
+      >
+        <Input
+          focusBorderColor="#53735E"
+          placeholder="Board Title"
+          style={{ marginBottom: "10px" }}
+          {...register("title", { required: true, maxLength: 18 })}
+        />
+        {errors.title?.type === "required" && (
+          <span style={{ color: "red" }}>This field is required!</span>
+        )}
+        {errors.title?.type === "maxLength" && (
+          <p style={{ color: "red" }} role="alert">
+            Max Length is 18 symbols
+          </p>
+        )}
+        <Footer style={{ display: "flex" }}>
+          <Button
+            size={"sm"}
+            onClick={() => {
+              setActiveAddList(!activeAddList);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            size={"sm"}
+            background="#53735E"
+            color="#F2F2F2"
+            _hover={{ color: "black", bg: "#F2F2F2" }}
+            ml={3}
+            type="submit"
+          >
+            Add List
+          </Button>
+        </Footer>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default AddListField;
